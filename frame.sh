51e0fc1192157520e1e2870c28084f01208ad4ed
tellurion-kanta <tellurion.kanata@gmail.com>
2024-11-22 14:59:16 +0900

    test 2

--- frame.sh
+++ frame.sh
@@ -1,7 +1,9 @@
 DIR=/home/demopc/Public/TDC/4D-Humans/outputs/
-file_path="/home/demopc/Public/TDC/dataset/single/test/debug/1204633/video_frames_jpg/"
+file_path="/home/demopc/Public/TDC/dataset/single/test/debug/1204633/video_frames_jpg"
+out_path="/home/demopc/Public/TDC/4D-Humans/outputs/_TMP/"
 
 rm -f "$DIR"/*.mp4
+rm -f "$DIR"/*.png
 rm -rf "$DIR"/results/*
 
-python track.py video.source=$file_path
+python track.py video.source=$file_path video.output_dir=$out_path
